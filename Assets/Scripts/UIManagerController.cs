eaa638bf66ee724e83573634dc7c955e324ea7e5
Roberto Glez <roberto_glez@outlook.com>
2019-03-12 23:19:47 -0600

    Avance

---
+++ Assets/Scripts/UIManagerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerController : MonoBehaviour {
+    public GameObject canvasMenu;
+    public GameObject canvasConfig;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void test()
+    {
+        Debug.Log("Este es un ejmplo");
+    }
+
+    public void goToConfig(){
+        canvasMenu.SetActive(false);
+        canvasConfig.SetActive(true);
+    }
+
+    public void goAtConfig() {
+        canvasMenu.SetActive(true);
+        canvasConfig.SetActive(false);
+    }
+}
